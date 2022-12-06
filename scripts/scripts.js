cfae23992b6f882afec862f56318d8843389f673
IrisQY3167 <irisxueee98@gmail.com>
2022-12-05 21:41:27 -0500

    image_update_beans

--- scripts/scripts.js
+++ scripts/scripts.js
@@ -1,1 +1,205 @@
 /* WRITE YOUR JS HERE... YOU MAY REQUIRE MORE THAN ONE JS FILE. IF SO SAVE IT SEPARATELY IN THE SCRIPTS DIRECTORY */
+
+const coffeeData = [
+    {
+      brand: 'La Colombe',
+      name: 'Corsica',
+      roast: 'dark',
+      flavor: 'baker\'s chocolate, red wine, and spices',
+      bean: 'a mix of different beans',
+      region: ['Brazil', 'Columbia', 'Honduras', 'Mexico'],
+      price: '$14/12oz',
+      img:''
+    },
+   {
+     brand: 'Just Bright',
+     name:'Light Roast',
+     roast:'light',
+     flavor:'crisp and clean',
+     bean:'Arabica',
+     region:'central and south America',
+     price:'$5.12/12oz',
+     img: 'images/just-bright.jpg'
+   },
+   {
+     brand:'Melitta',
+     name:'Colombian Supreme',
+     roast:'medium',
+     flavor:'french vanilla',
+     bean:'Arabica',
+     region:'Columbia',
+     price:'$6.49/11oz',
+     img:''
+    },
+   {
+     brand:'Death Wish',
+     name:'Organic Whole Bean Coffee',
+     roast:'dark',
+     flavor:'bold, dark chocolate and black cherry',
+     bean:['Arabica', 'Robusta'],
+     region:['coffee belt', 'others'],
+     price:'$17.48/16oz',
+     img:''
+    },
+   {
+     brand:'Superlost Coffee',
+     name:'Natural Process Coffee',
+     roast:'medium',
+     flavor:'dark chocolate, sweet peach, key lime pie',
+     bean:'a mix of different beans',
+     region:'Columbia',
+     price:'$15/8oz',
+     img:''
+    },
+   {
+     brand:'Lavazza',
+     name:'Espresso Italiano',
+     roast:'medium',
+     flavor:'fruit and flowers',
+     bean:'Arabica',
+     region:'central and south America',
+     price:'$8.49/16oz',
+     img:''
+    },
+   {
+     brand:"Peet's Coffee",
+     name:'Big Bang',
+     roast:'medium',
+     flavor:'fruit cup, milk chocolate, graham crust',
+     bean:'Arabica',
+     region:['Africa','Arabia', 'America'],
+     price:'$18.95/16oz',
+     img:''
+    },
+   {
+     brand:'Good & Gather Organic',
+     name:'Tierra del Sol',
+     roast:'medium',
+     flavor:'dark chocolate, toffee and warm spice',
+     bean:'Arabica',
+     region:'Mexico',
+     price:'$7.39/12oz',
+     img:''
+    },
+   {
+     brand:'Intelligentsia',
+     name:'House Blend',
+     roast:'light',
+     flavor:'apple, citrus and milk chocolate',
+     bean:'Arabica',
+     region:['Brazil', 'Costa Rica', 'El Salvador'],
+     price:'$16.5/12oz',
+     img:''
+    },
+   {
+     brand:'Copper Cow Coffee',
+     name:'Salted Caramel Ground Coffee',
+     roast:'medium',
+     flavor:'caramel, salty, chocolate and nutty',
+     bean:['Arabica', 'Robusta'],
+     region:'Vietnamese',
+     price:'$17/12oz',
+     img:''
+    },
+   {
+     brand:'Counter Culture Coffee',
+     name:'Big Trouble',
+     roast:'medium',
+     flavor:'caramel, nutty, dark chocolate',
+     bean:'a mix of different beans',
+     region:['Honduras', 'Mexico'],
+     price:'$13.42/12oz',
+     img:''
+    },
+   {
+     brand:'Equal Exchange',
+     name:'Organic French Roast Coffee',
+     roast:'dark',
+     flavor:'dark chocolate, sweet and fruity',
+     bean:'Arabica',
+     region:'Nicaragua',
+     price:'$6.38/10oz',
+     img:''
+    },
+   {
+     brand:'Dope Coffee',
+     name:'Organic Guatemalan',
+     roast:'medium',
+     flavor:'dark chocolate and apricot blossoms',
+     bean:'a mix of different beans',
+     region:'Guatemala',
+     price:'$17/12oz',
+     img:''
+    },
+   {
+     brand:"Seattle's Best Coffee",
+     name:'Post Alley Blend',
+     roast:'dark',
+     flavor:'bittersweet, toasty, decadent chocolate',
+     bean:'Arabica',
+     region:'Latin America',
+     price:'$12/12oz',
+     img:''
+    },
+   {
+     brand:'Bizzy Cold Brew',
+     name:'Smooth & Sweet',
+     roast:'medium',
+     flavor:'caramel, hazelnut',
+     bean:'Arabica',
+     region:['Guatemala', 'Peru', 'Nicaragua'],
+     price:'$14.99/16oz',
+     img:''
+    },
+   {
+     brand:'Fresh Roasted Coffee',
+     name:'Tanzanian Peaberry',
+     roast:'light',
+     flavor:'lemon, peach, and black tea',
+     bean:'Arabica',
+     region:'Tanzania',
+     price:'$13.95/12oz',
+     img:''
+    },
+   {
+     brand:'CoffeeZyme',
+     name:'Coffee Cake',
+     roast:'medium',
+     flavor:'cinnamon, vanilla',
+     bean:'a mix of different beans',
+     region:'Brazil',
+     price:'$14.99/16oz',
+     img:''
+    },
+   {
+     brand:'Shock',
+     name:'Shock Coffee',
+     roast:'medium',
+     flavor:'cinnamon, vanilla',
+     bean:'a mix of different beans',
+     region:'Brazil',
+     price:'$14.99/16oz',
+     img:''
+    },
+   {
+     brand:'Cafés Granell',
+     name:'Wild Kopi Luwak Coffee',
+     roast:'medium',
+     flavor:'smooth, earthy, nutty and with a slight hint of spice',
+     bean:'Arabica',
+     region:'Indonesia',
+     price:'$109.98/3.5oz',
+     img:''
+    },
+   {
+     brand:'CoffeeAM',
+     name:'Jamaica Blue Mountain Estate Coffee',
+     roast:'light',
+     flavor:'chocolate',
+     bean:'Arabica',
+     region:'Jamaica',
+     price:'$54.95/16oz',
+     img:''
+    },
+ ];
+
